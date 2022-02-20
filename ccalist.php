a8bcb6b736072082b70101afe5f7a0f7b5f7e7a0
PBJimm <20ftt1813@student.pb.edu.bn>
2022-02-20 19:02:02 +0800

    Update ccalist.php

--- ccalist.php
+++ ccalist.php
@@ -1,62 +1,7 @@
 <!DOCTYPE html>
 <html>
-<style>
-            table {
-                    font-family: arial, sans-serif;
-                    border-collapse: collapse;
-                    width: 80%;
-                    
-                }
-
-            td, th {
-                    border: 2px solid #dddddd;
-                    text-align: middle;
-                    padding: 8px;
-                    }
-
-            tr:nth-child(even) {
-                    background-color: #dddddd;
-                    }    
-    </style>
+>
     <body>
-    <table>
-            
-            <tr>
-                <th>Name of Club</th>
-                <th>Details of Club</th>
-                <th>Tick (If Interested)</th>
-            </tr>
-            <tr>
-                <td>Politeknik Brunei Touch Rugby</td>
-                <td>Are you up to the challenge?! Join us as we roar upon the field with the finest team that existed in Politeknik Brunei. We ensure you get the best basic knowledge and the fun with touch rugby</td>
-                <td><input type="checkbox" name="tick" value="Politeknik_Brunei_Touch_Rugby"></td>
-            </tr>
-            <tr>
-                <td>Let's Japan Club</td>
-                <td>Konichiwa! Here in Let's Japan Club we are going to teach you about the basic and fundamental knowledge about Japan's culture and tradition</td>
-                <td><input type="checkbox" name="tick" value="Let's_Japan_Club"></td>
-            </tr>
-            <tr>
-                <td>Politeknik Brunei Movie Club</td>
-                <td>Are you interested in short film making, but you don't know where to start? Then our club is your one stop centre. Come and join us as we are going the fundamental of script writtings, editting and stroyboard</td>
-                <td><input type="checkbox" name="tick" value="Politeknik_Brunei_Movie_Club"></td>
-            </tr>
-            <tr>
-                <td>Tae Kwon Do Club</td>
-                <td>For those who are interested in learning self-defense. This club not only teach you martial art but it also teach you self-decipline.</td>
-                <td><input type="checkbox" name="tick" value="Tae_Kwon_Do_Club"></td>
-            </tr>
-            <tr>
-                <td>Fishing Club</td>
-                <td>Hi all! Those who interested in fishing or someone who wants to know about this club can join us.</td>
-                <td><input type="checkbox" name="tick" value="Fishing_Club"></td>
-            </tr>
-            <tr>
-                <td>Football Club</td>
-                <td>Untuk mengganti Sir Alex Fergueson</td>
-                <td><input type="checkbox" name="tick" value="Football_Club"></td>
-            </tr>
 
-            </table>
     </body>
 </html>
