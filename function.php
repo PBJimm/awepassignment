0aa91bbea3787352b01b3f5ca51e4c781f09fa2a
PBJimm <20ftt1813@student.pb.edu.bn>
2022-02-20 17:02:22 +0800

    Pakai input saja semua

--- function.php
+++ function.php
@@ -3,6 +3,17 @@
     <body>
     <?php
 
+    $studentName = $_POST['studentName'];
+    $studentId = $_POST['studentId'];
+
+    if(empty($studentName)){
+        $empty_name = 'Please Insert Your Full Name';
+    }
+    if(empty($studentId)){
+        $empty_id = 'Please Insert Your Student ID';
+    }
+    include('index.php');
+
     
     ?>
     </body>
--- index.php
+++ index.php
@@ -6,30 +6,11 @@
         <form action="function.php" method="post">
         <form method="post">
             Student Name   : <input type="text" name="studentName" placeholder="Student Name"><br>
-                            
+                            <?php ?>
             Student ID     : <input type="number" name="studentId" placeholder="Student ID"><br>
-            Student E Mail : <input type="email" name="studentEmail" placeholder="Student E Mail"><br>
-            Student School : <select name="school">
-                                    <option>Schools</option>
-                                    <option value="School_of_Information_Communication_and_Technology">School of Information, Communication and Technology</option>
-                                    <option value="School_of_Business_Studies">School of Business Studies</option>
-                                    <option value="School_of_Health_Science">School of Health Science</option>
-                                    <option value="School_of_Science_and_Engineering">School of Science and Engineering</option>
-                                    <option value="School_of_Petroleum_Engineering">School of Petroleum Engineering</option>
-                            </select><br>
-            Courses        :
-            <br>
-            Web Development : <input type="radio" name="course" value="Web_Development">
-            <br>
-            Information System : <input type="radio" name="course" value="Information_System">
-            <br>
-            IT Network : <input type="radio" name="course" value="IT_Network">
-            <br>
-            Library : <input type="radio" name="course" value="Library">
-            <br>
-            Digital Media : <input type="radio" name="course" value="Digital_Media">
-            <br>
-
+            Student E Mail : <input type="email" name="studentEmail" placeholder="studentid@student.pb.edu.bn"><br>
+            Student School : <input type="text" name="studentSchool" placeholder="E.g SICT"><br>
+            Student Course : <input type="text" name="studentCourse" placeholder="E.g Diploma in Web Development"><br>
             <input type="submit" value="Submit" name="submit">
             <input type="reset" value="reset" name="reset">  
      
