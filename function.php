a447539e65f5f0ce5eeb4e53ddd8dd3c8b856750
PBJimm <20ftt1813@student.pb.edu.bn>
2022-02-20 17:09:38 +0800

    Update function.php

--- function.php
+++ function.php
@@ -5,6 +5,9 @@
 
     $studentName = $_POST['studentName'];
     $studentId = $_POST['studentId'];
+    $studentEmail =  $_POST['studentEmail'];
+    $studentSchool =  $_POST['studentSchool'];
+    $studentCourse =  $_POST['studentCourse'];
 
     if(empty($studentName)){
         $empty_name = 'Please Insert Your Full Name';
@@ -12,6 +15,16 @@
     if(empty($studentId)){
         $empty_id = 'Please Insert Your Student ID';
     }
+    if(empty($studentEmail)){
+        $empty_id = 'Please Insert Your Student Email';
+    }
+    if(empty($studentCourse)){
+        $empty_id = 'Please Insert Your Course';
+    }
+    if(empty($studentSchool)){
+        $empty_id = 'Please Insert Your School';
+    }
+
     include('index.php');
 
     
