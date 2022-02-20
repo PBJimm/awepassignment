2e0d4ab9d670dfc7528e67d9c91892d0a4987162
PBJimm <20ftt1813@student.pb.edu.bn>
2022-02-20 17:37:46 +0800

    Update function.php

--- function.php
+++ function.php
@@ -14,14 +14,22 @@
     } elseif (strlen($studentName) < 6){
         $empty_name = 'Your name should not be less than 6';
     }
+    // basically what it does here, arah elseif atu ia gtau kalau input name mu kurang daripada 6 then ia keluarkan the statement atu, strlen means string lenght
+    // tapinya take note, this only happens if kamu buat kurang daripada 6. sekiranya kalau kamu termiss enter nama atu then ia kan keluarkan please insert your fullname atu
+
     if(empty($studentId)){
         $empty_id = 'Please Insert Your Student ID';
     } elseif (strlen($studentId) < 6){
         $empty_id = 'Your Student ID should not be less than 6';
     }
+    // sama juanya disini kalau id kamu atu kurang daripada 6 then kira atu error lah jua
+    // sama juanya disini macam diatas kalau ketinggalan id ia keluarkan the insert your student id atu, and vice versa
+
+
     if(empty($studentEmail)){
         $empty_email = 'Please Insert Your Student Email';
     }
+
     if(empty($studentCourse)){
         $empty_course = 'Please Insert Your Course';
     } 
