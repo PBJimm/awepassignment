e5be5c18774a7867c60d0b4b9ffed3dc33bc7ceb
PBJimm <20ftt1813@student.pb.edu.bn>
2022-02-20 17:24:35 +0800

    Update function.php

--- function.php
+++ function.php
@@ -11,18 +11,26 @@
 
     if(empty($studentName)){
         $empty_name = 'Please Insert Your Full Name';
+    } elseif (strlen($studentName) < 3){
+        $empty_name = 'Your name should not be less than 3';
     }
     if(empty($studentId)){
         $empty_id = 'Please Insert Your Student ID';
+    } elseif (strlen($studentId) < 6){
+        $empty_id = 'Your Student ID should not be less than 6';
     }
     if(empty($studentEmail)){
         $empty_email = 'Please Insert Your Student Email';
     }
     if(empty($studentCourse)){
         $empty_course = 'Please Insert Your Course';
+    } elseif (strlen($studentCourse) < 6){
+        $empty_course = 'Your student course should not be less than 6'
     }
     if(empty($studentSchool)){
         $empty_school = 'Please Insert Your School';
+    } elseif (strlen($studentSchool) < 6){
+        $empty_school = 'Your  schoool name should not be less than 6'
     }
 
     include('index.php');
