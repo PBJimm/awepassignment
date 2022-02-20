0aafdc47d2a30149bcb014a9270f2a50daa83538
PBJimm <20ftt1813@student.pb.edu.bn>
2022-02-20 17:43:17 +0800

    Update function.php

--- function.php
+++ function.php
@@ -15,6 +15,7 @@
         $empty_name = 'Your name should not be less than 6';
     }
     // basically what it does here, arah elseif atu ia gtau kalau input name mu kurang daripada 6 then ia keluarkan the statement atu, strlen means string lenght
+    // the name yang $empty_name atu basically macam nama function, untuk di panggil function atu arah index.php
     // tapinya take note, this only happens if kamu buat kurang daripada 6. sekiranya kalau kamu termiss enter nama atu then ia kan keluarkan please insert your fullname atu
     // reference link -  https://youtu.be/MMHUxT1pSMg
 
@@ -24,6 +25,7 @@
         $empty_id = 'Your Student ID should not be less than 6';
     }
     // sama juanya disini kalau id kamu atu kurang daripada 6 then kira atu error lah jua
+    // the name yang $empty_id atu basically macam nama function, untuk di panggil function atu arah index.php
     // sama juanya disini macam diatas kalau ketinggalan id ia keluarkan the insert your student id atu, and vice versa
     // reference link -  https://youtu.be/MMHUxT1pSMg
 
@@ -31,14 +33,17 @@
     if(empty($studentEmail)){
         $empty_email = 'Please Insert Your Student Email';
     }
+    // the name yang $empty_email atu basically macam nama function, untuk di panggil function atu arah index.php
 
     if(empty($studentCourse)){
         $empty_course = 'Please Insert Your Course';
     } 
+    // the name yang $empty_course atu basically macam nama function, untuk di panggil function atu arah index.php
     
     if(empty($studentSchool)){
         $empty_school = 'Please Insert Your School';
     } 
+    // the name yang $empty_school atu basically macam nama function, untuk di panggil function atu arah index.php
 
     include('index.php');
 
